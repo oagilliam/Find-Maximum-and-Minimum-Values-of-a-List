579d3254268bf3483f90ebda437b5febc9c9e252
Olivia Gilliam <75933763+oagilliam@users.noreply.github.com>
2023-03-10 20:25:18 -0500

    Add files via upload
    Codewars problem

---
+++ Find-Maximum-and-Minimum-Values-of-a-List.py
@@ -0,0 +1,15 @@
+# Find Maximum and Minimum Values of a List
+
+# Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) 
+# that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+# My Solution
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
+
+print(minimum([-52, 56, 30, 29, -54, 0, -110]))
+print(maximum([4,6,2,1,9,63,-134,566]))
